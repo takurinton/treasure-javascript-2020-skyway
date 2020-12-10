cb547ded993673954f802839ea1952f44499880a
takurinton <takuirnton@gmail.com>
2020-12-10 17:07:09 +0900

    Updates

---
+++ precache-manifest.a684b27da4a76d7d3d9fb23df235ca0f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "926947a38a48eb1536c8df044799b7c1",
+    "url": "/treasure-javascript-2020-skyway/index.html"
+  },
+  {
+    "revision": "e8e48ee660b7e741c7d7",
+    "url": "/treasure-javascript-2020-skyway/static/js/2.ffa117f9.chunk.js"
+  },
+  {
+    "revision": "07a501d9903b06542e25152959ed155c",
+    "url": "/treasure-javascript-2020-skyway/static/js/2.ffa117f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5102dd3de1d631c1c797",
+    "url": "/treasure-javascript-2020-skyway/static/js/main.b57fe57c.chunk.js"
+  },
+  {
+    "revision": "802a800030ea504102fb",
+    "url": "/treasure-javascript-2020-skyway/static/js/runtime-main.ffbad9b5.js"
+  }
+]);
